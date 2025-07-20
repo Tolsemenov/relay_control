d939da560dece10089ecd02fc72a1996975846f2
SemenovAO <tol.semenoff@mail.ru>
2025-07-20 18:52:03 +0300

    добавил wifi_setup

--- app/main.py
+++ app/main.py
@@ -53,4 +53,9 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        pass
